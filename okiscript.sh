2488dedd9974df3ba23909a57a904b8d16a69a85
okechukwu ikwunze <52090888+oikwunze@users.noreply.github.com>
2022-01-12 12:22:35 -0600

    added scripting file

---
+++ okiscript.sh
@@ -0,0 +1,46 @@
+ echo ====================================================
+          echo 1. What is in this directory?
+          ls -a
+          echo
+          echo ====================================================
+          echo 2. Is Java installed?
+          java -version
+          echo
+          echo ====================================================
+          echo 3. Is Git installed?
+          git --version
+          echo ====================================================
+          echo 4. What build tools are installed?
+          mvn --version
+          echo ====================================================
+          gradle --version
+          echo ====================================================
+          ant -version
+          echo
+          echo ====================================================
+          echo 5. Where is the Android SDK Root?
+          echo $ANDROID_SDK_ROOT	
+          echo
+          echo ====================================================
+          echo 6. Where are the Selenium jars?
+          echo $SELENIUM_JAR_PATH
+          echo
+          echo ====================================================
+          echo 7. What is the workspace location
+          echo $RUNNER_WORKSPACE
+          echo
+          echo ====================================================
+          echo 8. Who is running this script?
+          whoami
+          echo ====================================================
+          echo 9. How is the disc laid out?
+          df
+          echo ====================================================
+          echo 10. What environment variables are avaialable?
+          env
+          echo
+          echo ====================================================
+          echo my present working directory?
+          pwd
+          echo
+          echo ====================================================
